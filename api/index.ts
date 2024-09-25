4766a2de0f7ca70edded0a415c0bb4e107db6601
Nick Vigier <nvigier46@gmail.com>
2024-09-25 14:18:01 +0100

    testing with api folder

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-export default async function handler(req: Request) {
+export async function GET(req: Request) {
   return new Response('Hello, world!', {
     headers: { 'content-type': 'text/plain' },
     status: 200,
